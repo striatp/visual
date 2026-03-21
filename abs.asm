1de674344fd66b473dd09c6132e70c69eb5315ee
Striatp <sebastien.garcia@mail.com>
2026-03-22 00:58:32 +0100

    Update abs.asm

--- abs.asm
+++ abs.asm
@@ -1,11 +1,11 @@
-        MOV     R0, #-8
+        MOV     R0, #-8         ; MOCK
         BL      ABS
         END
 
 ABS
-        CMP     R0, #0
-        BGE     ABS_RET
-        RSB     R0, R0, #0
+        CMP     R0, #0          ; IS R0 NEGATIVE
+        BGE     INNER_ABS_RET   ; NO, NO CHANGES NEEDED
+        RSB     R0, R0, #0      ; PERFOM 'R0 = 0 - R0', IF NEGATIVE
 
-ABS_RET
-        MOV     PC, LR
+INNER_ABS_RET
+        MOV     PC, LR          ; RETURN TO CALLER
