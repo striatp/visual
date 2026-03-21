382eb788530c0ba845a10ec10f970e655ef01fe7
Striatp <sebastien.garcia@mail.com>
2026-03-22 00:59:13 +0100

    Fix formatting in ABS function assembly code

--- abs.asm
+++ abs.asm
@@ -4,7 +4,7 @@
 
 ABS
         CMP     R0, #0          ; IS R0 NEGATIVE
-        BGE     ABS_RET   ; NO, NO CHANGES NEEDED
+        BGE     ABS_RET         ; NO, NO CHANGES NEEDED
         RSB     R0, R0, #0      ; PERFOM 'R0 = 0 - R0', IF NEGATIVE
 
 ABS_RET
