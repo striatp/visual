82e2cc94737b1087824eb54b7a662f54326fcb6d
Striatp <sebastien.garcia@mail.com>
2026-03-22 00:58:49 +0100

    Update abs.asm

--- abs.asm
+++ abs.asm
@@ -4,8 +4,8 @@
 
 ABS
         CMP     R0, #0          ; IS R0 NEGATIVE
-        BGE     INNER_ABS_RET   ; NO, NO CHANGES NEEDED
+        BGE     ABS_RET   ; NO, NO CHANGES NEEDED
         RSB     R0, R0, #0      ; PERFOM 'R0 = 0 - R0', IF NEGATIVE
 
-INNER_ABS_RET
+ABS_RET
         MOV     PC, LR          ; RETURN TO CALLER
