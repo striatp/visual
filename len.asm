6d8364ef24be2e9e64b87411a4a21cc858be8f98
Striatp <sebastien.garcia@mail.com>
2026-03-22 09:46:01 +0100

    feat: refactor LEN function for clarity and stack management

--- len.asm
+++ len.asm
@@ -3,28 +3,26 @@
         END
 
 LEN
-        SUB     R13, R13, #8    ; MAKE ROOM ON THE STACK
-        STR     R2, [R13, #4]
-        STR     R3, [R13]
+        SUB     R13, R13, #8    ; ALLOCATE STACK FRAME
+        STR     R2, [R13, #4]   ; SAVE R2
+        STR     R3, [R13]       ; SAVE R3
 
-        MOV     R1, #0          ; RESULT STORED IN R1
+        MOV     R1, #0          ; LENGTH COUNTER (RETURNED VALUE)
         MOV     R2, R0          ; WALK POINTER (ADDRESS)
 
 _LEN_INNER
         LDR     R3, [R2]        ; GET CURRENT VALUE OF WALK POINTER
         CMP     R3, #0          ; IS EQUAL TO ZERO?
-        BEQ     _LEN_RET        ; NULL-TERMINATOR FOUND, RETURN
+        BEQ     _LEN_RET        ; YES, NULL-TERMINATOR FOUND, RETURN
 
         ADD     R1, R1, #1      ; ELSE INCREMENT R1
-        ADD     R2, R2, #4      ; ADD 4 BYTES (WORD) TO WALK POINTER
-                                ; TO GET THE NEXT ITEM OF THE ARRAY
+        ADD     R2, R2, #4      ; ADVANCE POINTER BY 1 WORD
         B       _LEN_INNER
 
 _LEN_RET
         LDR     R3, [R13]
         LDR     R2, [R13, #4]
-        ADD     R13, R13, #8    ; FREE THE ROOM MADE ON THE STACK
-                                ; AND PUT THEIR RESPECTIVE VALUES BACK
+        ADD     R13, R13, #8    ; FREE STACK FRAME
         MOV     PC, LR          ; RETURN TO CALLER
 
 DAT
